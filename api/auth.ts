06485492146ba1b77aefc64d4007696a1f7c4b52
chary <920868587@qq.com>
2023-08-02 19:03:28 +0800

    改！

--- api/auth.ts
+++ api/auth.ts
@@ -1,7 +1,8 @@
 import Ably from "ably/promises";
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 export default async function handler(req:VercelRequest, res:VercelResponse) {
-  const client = new Ably.Realtime(process.env.ABLY_API_KEY as string);
+  // const client = new Ably.Realtime(process.env.ABLY_API_KEY as string);
+  const client = new Ably.Realtime('b5_GaA.9d4a0Q:ni423V7pOJCKSTPuUV3eTFTnWyv-s2gBHNWX6qT2HXM');
   const tokenRequestData = await client.auth.createTokenRequest({
     clientId: "story-talk" + req.query['id'],
   });
