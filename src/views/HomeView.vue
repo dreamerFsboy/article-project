18c9fd5f9e26e2f3fa1be3add04554c4c5a855c4
chary <920868587@qq.com>
2023-08-02 20:54:47 +0800

    testbegin

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -11,17 +11,31 @@ import { shallowRef } from 'vue';
 import Ably from 'ably'
 import TheWelcome from '../components/TheWelcome.vue'
 const text = shallowRef('')
-let realtime
+let realtime: Ably.Realtime
+let channel: Ably.Types.RealtimeChannelCallbacks
 const Connect = () => {
-  console.log(text.value)
-  fetch('/api/auth?id='+text.value).then(res => res.json()).then(res => {
+  console.log(text.value);
+  (!realtime)&&fetch('/api/auth?id='+text.value).then(res => res.json()).then(res => {
     console.log(res)
-    // realtime = new Ably.Realtime({
-    //   authUrl: '/api/auth',
-    //   authMethod: 'POST',
-    //   authParams: 
-    // })
+    realtime = new Ably.Realtime({
+      authUrl: '/api/auth',
+      authMethod: 'POST',
+      authParams: res,
+    })
+    console.log(realtime)
+    channel = realtime.channels.get('annonce')
+    console.log(channel)
+    channel.attach((e) => {
+      console.log('"chatroom" exists and is now available globally in every datacenter');
+    })
+    channel.on('detached', function(stateChange) {
+      console.log('detached from the channel ' + channel.name);
+    })
+    channel.subscribe(function(message) {
+      console.log(message);
+    });
   })
+  channel&&channel.publish('example', text.value);
 }
 </script>
 
